8c88496bd50a176530987a2c943e3dd77fbd746e
Mateusz <mateo@webdev.webdev>
2017-02-10 23:37:36 +0100

    small refactoring

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,23 +1,8 @@
-import rest from 'rest'
-import mime from 'rest/interceptor/mime'
+import configureApi from './configureApi'
 
-const restApiClient = (store) => {
-    const client = rest.wrap(mime, { mime: 'application/json' })
-    const state = store.getState()
-    return (params) => {
-        const headers = state.session.headers
-        const reqParams = {...params, headers}
-        return new Promise((resolve, reject) => {
-            client(reqParams).then(
-                response => {
-                    store.dispatch({type: 'ACTION', headers: response.headers})
-                    resolve(response.entity)
-                }
-            ).catch(error => {
-                rejct(error)
-            })
-        })
+const api = {
+    this._path = '/'
+    collection(name) {
+
     }
 }
-
-export default restApiClient
--- src/api/configureApi.js
+++ src/api/configureApi.js
@@ -1,8 +1,27 @@
-import apiClient from './apiClient'
+import rest from 'rest'
+import mime from 'rest/interceptor/mime'
 
 const apiConfiguration =  {
     configureApi(store) {
-        this.api = apiClient(store)
+        this.api = restApiClient(store)
+    }
+}
+
+const restApiClient = ({ getState, dispatch }) => {
+    const client = rest.wrap(mime, { mime: 'application/json' })
+    return (params) => {
+        const headers = getState().session.headers
+        const reqParams = {...params, headers}
+        return new Promise((resolve, reject) => {
+            client(reqParams).then(
+                response => {
+                    dispatch({type: 'ACTION', headers: response.headers})
+                    resolve(response.entity)
+                }
+            ).catch(error => {
+                rejct(error)
+            })
+        })
     }
 }
 export default apiConfiguration
